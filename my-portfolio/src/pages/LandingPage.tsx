67b5858cfa504a82f5fb745ca92300f589a70bfb
AazeemVS <av7304@rit.edu>
2025-06-23 12:13:36 -0400

    formatted the my skills text and the button in a grid layout for the my skills component

--- my-portfolio/src/pages/LandingPage.tsx
+++ my-portfolio/src/pages/LandingPage.tsx
@@ -151,11 +151,29 @@ const LandingPage: React.FC = () => (
 
     <section className="bg-[#0f172a] text-card-foreground flex flex-col gap-6 rounded-xl border-2 border-solid border-[#27272a] py-6 shadow-sm w-[45%] mt-20 mx-auto relative flex flex-col justify-center overflow-hidden">
       <div className="w-full max-w-6xl mx-auto">
-        <div className="flex flex-col justify-center [&>*]:pb-4 ml-[2rem]">
+        <div className="flex flex-col justify-center [&>*]:pb-4 ">
           {/* Title */}
-          <div className="text-left">
-            <h2 className="text-white text-5xl font-bold">My Skills</h2>
-            <div></div>
+          <div className="grid grid-cols-2 ml-[2rem] mr-[2rem]">
+            <div className="text-left">
+              <h2 className="text-white text-5xl font-bold">My Skills</h2>
+            </div>
+            <div>
+              <div className="text-right ">
+                {/* Button which appears on hover which allows the user to click onto a sub page about the project*/}
+                <button
+                  className="
+           
+            transition-all duration-300
+            text-xs bg-white text-gray-600
+            px-4 py-2 rounded-lg border border-black
+            font-semibold hover:bg-slate-300
+            ml-[1%]
+          "
+                >
+                  Learn more →
+                </button>{" "}
+              </div>
+            </div>
           </div>
           <SkillsScroller
             icons={[HTMLIcon, CSSIcon, JSIcon, TSIcon, ReactIcon]}
