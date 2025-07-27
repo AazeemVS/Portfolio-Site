36591cc9fd691dd1719f02e2530fc53ec97b477d
AazeemVS <av7304@rit.edu>
2025-07-26 23:29:24 -0400

    Added responsiveness to the contact me page

--- my-portfolio/src/pages/Projects.tsx
+++ my-portfolio/src/pages/Projects.tsx
@@ -7,97 +7,14 @@ import { Card } from "@/components/ui/card";
 import HTMLIcon from "@/components/ui/icons/HTMLIcon";
 import CSSIcon from "@/components/ui/icons/CSSIcon";
 import JSIcon from "@/components/ui/icons/jsIcon";
-import JavaIcon from "@/components/ui/icons/javaIcon";
 
-import { useNavigate, useLocation } from "react-router-dom";
-
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuLink,
-} from "@/components/ui/navigation-menu";
-import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
 //Images
 import SBESCIImg from "@/../images/SBESCI_site_pic.png";
 import CatchFrenzyImg from "@/../images/Catch_Frenzy_Site_Pic2.png";
 import WorkoutAppImg from "@/../images/WorkoutAppHomePage.png";
 import UnexpectedEscapesHomePage from "@/../images/Unexpected_Escapes_homeImg.png";
-
-function NavBar() {
-  const location = useLocation();
-  const underlineVariants = {
-    rest: { width: 0 },
-    hover: { width: "100%" },
-    active: { width: "100%" },
-  };
-  const underlineTransition = { duration: 0.3, ease: "easeOut" };
-
-  const scrollToSection = (sectionId: string) => {
-    const element = document.getElementById(sectionId);
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
-
-  return (
-    <div className="mt-5 flex justify-center">
-      <div className="max-w-4xl px-4">
-        <NavigationMenu className="w-full">
-          <NavigationMenuList className="flex justify-center">
-            <NavigationMenuItem>
-              <div className="flex space-x-6 px-6 py-2 border-2 border-[#27272a] rounded-full">
-                {[
-                  ["Web Projects", "web-projects"],
-                  ["Game Projects", "game-projects"],
-                ].map(([label, sectionId]) => (
-                  <NavigationMenuItem key={label}>
-                    <motion.div
-                      className="relative"
-                      initial="rest"
-                      whileHover="hover"
-                      whileTap={{ scale: 0.95 }}
-                    >
-                      <NavigationMenuLink
-                        className={`text-lg font-bold whitespace-nowrap px-4 py-1 ${
-                          location.hash === `#${sectionId}`
-                            ? "text-cyan-700"
-                            : "text-white"
-                        }`}
-                        onClick={() => scrollToSection(sectionId)}
-                      >
-                        {label}
-                        <motion.span
-                          className="absolute bottom-0 left-0 h-0.5 bg-cyan-700"
-                          variants={underlineVariants}
-                          initial={
-                            location.hash === `#${sectionId}`
-                              ? "active"
-                              : "rest"
-                          }
-                          animate={
-                            location.hash === `#${sectionId}`
-                              ? "active"
-                              : "rest"
-                          }
-                          transition={underlineTransition}
-                        />
-                      </NavigationMenuLink>
-                    </motion.div>
-                  </NavigationMenuItem>
-                ))}
-              </div>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
-    </div>
-  );
-}
 
 const Projects: React.FC = () => {
   const navigate = useNavigate();
@@ -123,7 +40,6 @@ const Projects: React.FC = () => {
         icons={[
           <AngularIcon key="1" />,
           <TSIcon key="2" />,
-          <JavaIcon key="3" />,
           <HTMLIcon key="4" />,
           <CSSIcon key="5" />,
         ]}
