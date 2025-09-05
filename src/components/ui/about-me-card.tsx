6cb8103e3a6c13a3839efdfda2b31b0d6dcfe174
AazeemVS <av7304@rit.edu>
2025-09-04 20:59:39 -0400

    took out location of my about me card

--- src/components/ui/about-me-card.tsx
+++ src/components/ui/about-me-card.tsx
@@ -1,10 +1,4 @@
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import {
   EnvelopeClosedIcon,
   GitHubLogoIcon,
@@ -62,28 +56,6 @@ const AbtMe = () => {
             </div>
           </div>
         </CardTitle>
-        <CardDescription>
-          <div className="flex items-center gap-2 mt-2 sm:mt-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="flex-shrink-0"
-            >
-              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-              <circle cx="12" cy="10" r="3"></circle>
-            </svg>
-            <p className="text-gray-400 text-sm sm:text-base">
-              Housing available in Falls Church, VA & Cambridge, MA
-            </p>
-          </div>
-        </CardDescription>
       </CardHeader>
       <CardContent>
         <p className="text-sm sm:text-base">
